3996098a1f3ffe464900f5071c15c6ff88192a44
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-11-05 23:01:33 -0300

    validaciones para registrar un usuario

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,27 +1,34 @@
+import { validationResult } from "express-validator";
 import Usuario from "../models/usuario";
 
 export const registrarUsuario = async (req, res) => {
-    try {
-        const usuarioNuevo = new Usuario(req.body);
-        await usuarioNuevo.save();
-        res.status(201).json({
-          mensaje: "Se registró el usuario",
-        });
-    } catch (error) {
-      console.log(error);
-      res.status(404).json({
-        mensaje: "Error al regsitrar el usuario",
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errores: errors.array(),
       });
     }
+    const usuarioNuevo = new Usuario(req.body);
+    await usuarioNuevo.save();
+    res.status(201).json({
+      mensaje: "Se registró el usuario",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al regsitrar el usuario",
+    });
+  }
 };
 
 export const login = async (req, res) => {
-    try {
-      const usuarios = await Usuario.find();
-      res.status(200).json(usuarios);
-    } catch (error) {
-      res.status(400).json({
-        mensaje: "Error al buscar los usuarios",
-      });
-    }
-  };
+  try {
+    const usuarios = await Usuario.find();
+    res.status(200).json(usuarios);
+  } catch (error) {
+    res.status(400).json({
+      mensaje: "Error al buscar los usuarios",
+    });
+  }
+};
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,9 +1,35 @@
 import { Router } from "express";
 import { login, registrarUsuario } from "../controllers/usuarios.controllers";
+import { check } from "express-validator";
 
 const router = Router();
 
-router.route("/registrar").post(registrarUsuario)
+router
+  .route("/registrar")
+    .post(
+        [
+            check("nombre")
+              .notEmpty() 
+              .withMessage("Este campo es obligatorio")
+              .isLength({min: 3})
+              .withMessage("El mínimo de caracteres es de 3"),
+            check("apellido")
+              .notEmpty() 
+              .withMessage("Este campo es obligatorio")
+              .isLength({min: 3})
+              .withMessage("El mínimo de caracteres es de 3"),
+            check("email")
+              .notEmpty() 
+              .withMessage("Este campo es obligatorio")
+              .matches(/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/)
+              .withMessage("Debe ingresar un email válido"),
+            check("password")
+              .notEmpty() 
+              .withMessage("Este campo es obligatorio")
+              .matches(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/)
+              .withMessage("Entre 8 y 16 caracteres, al menos un dígito, al menos una mayúscula, al menos una minúscula y NO puede tener otros símbolos.")
+        ],
+    registrarUsuario)
 
 router.route("/login").get(login)
 
